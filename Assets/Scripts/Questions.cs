dbbc7a72fc6b13e1e56c75405689ed368c5f3aa2
Harun Emrecan Karabağ <harunemrecankarabag@gmail.com>
2024-03-26 13:11:41 +0300

    Try to get flags pictures from the database. It's not working yet.

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -1,18 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Data;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Mono.Data.Sqlite;
+using System.Collections;
+using UnityEngine.Networking;
 
 public class Questions : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    Image soruResmi;
+
+    [SerializeField]
+    TextMeshProUGUI secenekAText;
+    [SerializeField]
+    TextMeshProUGUI secenekBText;
+    [SerializeField]
+    TextMeshProUGUI secenekCText;
+    [SerializeField]
+    TextMeshProUGUI secenekDText;
+
+    [SerializeField]
+    Button secenekAButton;
+    [SerializeField]
+    Button secenekBButton;
+    [SerializeField]
+    Button secenekCButton;
+    [SerializeField]
+    Button secenekDButton;
+
+    private string connectionString = "URI=file:" + Application.dataPath + "/Plugins/flags2.db";
+
+    private string dogruCevap;
+
     void Start()
     {
-        
+        StartCoroutine(LoadQuestion());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator LoadQuestion()
     {
-        
+        using (var dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            // Rastgele bir soru seçme
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT * FROM flags ORDER BY RANDOM() LIMIT 1"; // Rastgele bir soru se�me
+                dbCmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(reader.GetString(3)))
+                        {
+                            yield return www.SendWebRequest();
+
+                            if (www.result == UnityWebRequest.Result.Success)
+                            {
+                                Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+                                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+                                soruResmi.sprite = sprite;
+
+                                Debug.Log("Soru yüklendi!");
+                            }
+                            else
+                            {
+                                Debug.Log("Resim yükleme hatası: " + www.error);
+                            }
+                        }
+                        Debug.Log("Soru Url: " + reader.GetString(3));
+                        dogruCevap = reader.GetString(0);
+                        secenekAText.text = "Amerika";
+                        secenekBText.text = "Almanya";
+                        secenekCText.text = "Fransa";
+                        secenekDText.text = "İngiltere";
+                    }
+                }
+            }
+
+            dbConnection.Close();
+        }
+    }
+
+    public void CheckAnswer(string secilenCevap)
+    {
+        if (secilenCevap == dogruCevap)
+        {
+            Debug.Log("Doğru cevap!");
+        }
+        else
+        {
+            Debug.Log("Yanlış cevap! Doğru cevap: " + dogruCevap);
+        }
     }
 }
