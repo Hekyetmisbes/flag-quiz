cbdc017af36ec110ce6d923f5b89f86bd904848a
Harun Emrecan Karabağ <harunemrecankarabag@gmail.com>
2024-04-20 17:13:14 +0300

    The gameplay is finished. Gameover screen is added. Settings button and scene is deleted. The database is updated. The correct and wrong answer sound effects are added. Codes are cleaned. The game is not working on the mobile device. Database is not working on the mobile device. Flags are not downloaded from the database.

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,13 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    // Play button
     public void PlayButton() => SceneManager.LoadScene("Game");
 
+    // Menu button
     public void MenuButton() => SceneManager.LoadScene("MainMenu");
 
+    // Settings button
     public void SettingsButton() => SceneManager.LoadScene("Settings");
+
+    // Restart button
+    public void RestartButton()
+    {
+        this.gameObject.SetActive(false);
+        SceneManager.LoadScene("Game");
+    }
 }
--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -5,35 +5,51 @@ using UnityEngine.UI;
 using Mono.Data.Sqlite;
 using System.Collections;
 using UnityEngine.Networking;
-using Unity.VisualScripting;
-using System.Net;
 
 public class Questions : MonoBehaviour
 {
     [SerializeField]
-    Image soruResmi;
+    Image questionImage;
+    [SerializeField]
+    Image gameOverImage;
 
     [SerializeField]
-    TextMeshProUGUI secenekAText;
+    TextMeshProUGUI chooseAText;
     [SerializeField]
-    TextMeshProUGUI secenekBText;
+    TextMeshProUGUI chooseBText;
     [SerializeField]
-    TextMeshProUGUI secenekCText;
+    TextMeshProUGUI chooseCText;
     [SerializeField]
-    TextMeshProUGUI secenekDText;
+    TextMeshProUGUI chooseDText;
+    [SerializeField]
+    TextMeshProUGUI gameOverText;
+    [SerializeField]
+    TextMeshProUGUI scoreText;
 
     [SerializeField]
-    Button secenekAButton;
+    Button chooseAButton;
     [SerializeField]
-    Button secenekBButton;
+    Button chooseBButton;
     [SerializeField]
-    Button secenekCButton;
+    Button chooseCButton;
     [SerializeField]
-    Button secenekDButton;
+    Button chooseDButton;
 
+    [SerializeField]
+    AudioSource CorrectSound;
+
+    [SerializeField]
+    AudioSource WrongSound;
+
+    int score = 0;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private string connectionString = "URI=file:" + Application.persistentDataPath + "/flags2.db";
+#else
     private string connectionString = "URI=file:" + Application.dataPath + "/Plugins/flags2.db";
+#endif
 
-    private string dogruCevap;
+    private string correctAnswer;
 
     void Start()
     {
@@ -42,35 +58,72 @@ public class Questions : MonoBehaviour
 
     public IEnumerator LoadQuestion()
     {
+        chooseAButton.interactable = true;
+        chooseBButton.interactable = true;
+        chooseCButton.interactable = true;
+        chooseDButton.interactable = true;
         using (var dbConnection = new SqliteConnection(connectionString))
         {
             dbConnection.Open();
 
-            // Rastgele bir soru seçme
+            // Choose a random question
             using (IDbCommand dbCmd = dbConnection.CreateCommand())
             {
-                string sqlQuery = "SELECT * FROM flags ORDER BY RANDOM() LIMIT 1"; // Rastgele bir soru seçme
+                string sqlQuery = "SELECT * FROM flags ORDER BY RANDOM() LIMIT 1";
                 dbCmd.CommandText = sqlQuery;
 
                 using (IDataReader reader = dbCmd.ExecuteReader())
                 {
                     if (reader.Read())
                     {
-                        using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture("https://flagcdn.com/h240/bi.png"))
+                        string imageUrl = "https://flagcdn.com/h240/" + reader.GetString(1) + ".png"; // Get the image URL
+                        correctAnswer = reader.GetString(0); // Select the correct answer from the database according to the country code
+                        Debug.Log(reader.GetString(1));
+                        Debug.Log(correctAnswer);
+
+                        // Randomly select the correct answer and other options
+                        int randomsayi = Random.Range(0, 4);
+
+                        switch (randomsayi)
                         {
-                            // Web isteği yap
+                            case 0:
+                                chooseAText.text = reader.GetString(0);
+                                chooseBText.text = GetRandomCountryName();
+                                chooseCText.text = GetRandomCountryName();
+                                chooseDText.text = GetRandomCountryName();
+                                break;
+                            case 1:
+                                chooseAText.text = GetRandomCountryName();
+                                chooseBText.text = reader.GetString(0);
+                                chooseCText.text = GetRandomCountryName();
+                                chooseDText.text = GetRandomCountryName();
+                                break;
+                            case 2:
+                                chooseAText.text = GetRandomCountryName();
+                                chooseBText.text = GetRandomCountryName();
+                                chooseCText.text = reader.GetString(0);
+                                chooseDText.text = GetRandomCountryName();
+                                break;
+                            case 3:
+                                chooseAText.text = GetRandomCountryName();
+                                chooseBText.text = GetRandomCountryName();
+                                chooseCText.text = GetRandomCountryName();
+                                chooseDText.text = reader.GetString(0);
+                                break;
+                        }
+
+                        // Load the image from the URL
+                        using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(imageUrl))
+                        {
                             yield return webRequest.SendWebRequest();
 
-                            // Hata kontrolü
                             if (webRequest.result == UnityWebRequest.Result.Success)
                             {
-                                // Texture'ı al ve RawImage'a at
                                 Texture2D texture = DownloadHandlerTexture.GetContent(webRequest);
 
                                 Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
 
-                                // RawImage'a sprite'ı at
-                                soruResmi.sprite = sprite;
+                                questionImage.sprite = sprite;
 
                             }
                             else
@@ -78,32 +131,78 @@ public class Questions : MonoBehaviour
                                 Debug.LogError("Error downloading image: " + webRequest.error);
                             }
                         }
-
-                        Debug.Log("Soru Url: " + reader.GetString(3));
-                        dogruCevap = reader.GetString(0);
-                        secenekAText.text = "Amerika";
-                        secenekBText.text = "Almanya";
-                        secenekCText.text = "Fransa";
-                        secenekDText.text = "İngiltere";
-
-
                     }
                 }
             }
-
             dbConnection.Close();
         }
     }
 
-    public void CheckAnswer(string secilenCevap)
+    // Check the answer
+    public void CheckAnswer()
     {
-        if (secilenCevap == dogruCevap)
+        // If the selected answer is correct
+        if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponentInChildren<TextMeshProUGUI>().text == correctAnswer)
         {
-            Debug.Log("Doğru cevap!");
+            PlayCorrectSound();
+            score++;
+            scoreText.text = "Score: " + score;
+            StartCoroutine(LoadQuestion());
         }
+        // If the selected answer is wrong
         else
         {
-            Debug.Log("Yanlış cevap! Doğru cevap: " + dogruCevap);
+            chooseAButton.interactable = false;
+            chooseBButton.interactable = false;
+            chooseCButton.interactable = false;
+            chooseDButton.interactable = false;
+            PlayWrongSound();
+            if (score > PlayerPrefs.GetInt("HighScore"))
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+            gameOverText.text = "Game Over\r\n Answer: " + correctAnswer + "\r\nScore : " + score + "\r\nHigh Score: " + PlayerPrefs.GetInt("HighScore");
+            gameOverImage.gameObject.SetActive(true);
         }
+    }
+
+    // Get a random country name from the database for the wrong answers
+    string GetRandomCountryName()
+    {
+        using (var dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT countryname FROM flags ORDER BY RANDOM() LIMIT 1";
+                dbCmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        if (reader.GetString(0) != correctAnswer)
+                        {
+                            return reader.GetString(0);
+                        }
+                    }
+                }
+            }
+            dbConnection.Close();
+        }
+        return null;
+    }
+
+    // Play the correct sound
+    void PlayCorrectSound()
+    {
+        CorrectSound.Play();
+    }
+
+    // Play the wrong sound
+    void PlayWrongSound()
+    {
+        WrongSound.Play();
     }
 }
